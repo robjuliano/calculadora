f3e41a026e08cb62f20ad8a4e11cbd01cf7de442
robjuliano <robsonj2011@gmail.com>
2021-02-26 20:05:22 -0300

    Fix buttons side

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ import './Button.css'
 
 export default props =>{
     let classes ='button'
-    classes += props.operation ? 'operation' : ''
-    classes += props.double ? 'double' : ''
-    classes += props.triple ? 'triple' : ''
+    classes += props.operation ? ' operation' : ''
+    classes += props.double ? ' double' : ''
+    classes += props.triple ? ' triple' : ''
 
     return (
         <button
